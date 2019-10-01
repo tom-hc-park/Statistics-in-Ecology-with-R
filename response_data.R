701bf06b46a51c89b310e7922588028ff0aa1944
Amelia Hesketh <avhesketh@gmail.com>
2019-10-01 15:47:11 -0700

    annotated codes

--- response_data.R
+++ response_data.R
@@ -3,7 +3,6 @@ library(tidyverse)
 setwd("/Users/Amelia/Documents/UBC/R_projects/STAT506H")
 
 # read in data
-
 growth_0 <- read_csv("growth_data_zero.csv")
 
 growth_may <- read_csv("growth_data_may.csv")
@@ -16,42 +15,54 @@ growth_nov <- read_csv("growth_data_nov.csv")
 
 growth_1 <- read_csv("growth_data_march.csv")
 
+# concatenate the same data for fifty oysters into a longer data frame
 for (i in 1:6){
   growth_0 <- growth_0 %>% 
     dplyr::bind_rows(growth_0)
 }
 
+# trim the data down to a length of 2500, since we to simulate data for each bag within subsite & site
 growth_0 <- growth_0 %>% 
   slice(1:2500)
 
+# rename the variable outplant time to make it consistent for later merging
 growth_0 <- growth_0 %>% 
   rename(outplant_time = "outplant time")
 
+# create a character vector for site
 site <- c(rep("A", times = 500), rep("B", times = 500),
            rep("C", times = 500), rep("D", times = 500),
            rep("E", times = 500)) 
 
+# create a character vector for beach & raft subsites
 b_r <- rep(c(rep("b", times = 250), rep("r", times = 250)), times = 5)
 
+# create a vector for each of the five bags within each subsite & site
 bag <- rep(c(rep("1", times = 50),rep("2", times = 50),
           rep("3", times = 50), rep("4", times = 50),
           rep("5", times = 50)), times = 10)
 
+# create the site, subsite, and bag columns by joining these three vectors
 front_end <- cbind(site,b_r,bag)
 
+# change to a dataframe
 front_end <- as.data.frame(front_end)
 
+# bind with the cloned response data
 x <- cbind(front_end, growth_0)
 
+# select the columns of interest (in this case, only shell length)
 growth_0 <- x %>% 
   dplyr::select(1:4, 7)
 
+# change variables to factors
 growth_0$site <- as.factor(growth_0$site)
 growth_0$bag <- as.factor(growth_0$bag)
 growth_0$b_r <- as.factor(growth_0$b_r)
 
-## join rest of data
+# join with the rest of the data
 
+# first rename problematic columns
 growth_july <- growth_july %>% 
   rename("site" = "Site")
 
@@ -61,6 +72,7 @@ growth_1 <- growth_1 %>%
 growth_sept <- growth_sept %>% 
   rename("shell_length" = "length")
 
+# now join everything and select meaningful variables
 growth_full <- growth_may %>% 
   full_join(growth_july) %>% 
   full_join(growth_sept) %>% 
@@ -68,30 +80,39 @@ growth_full <- growth_may %>%
   full_join(growth_1) %>% 
   select(1:3, 5, 10)
 
+# change things to factors to allow further joining with zero data
 growth_full$site <- as.factor(growth_full$site)
 growth_full$bag <- as.factor(growth_full$bag)
 growth_full$b_r <- as.factor(growth_full$b_r)
 growth_full$outplant_time <- as.numeric(growth_full$outplant_time)
 
+# join with zero data
 growth <- growth_full %>% 
   full_join(growth_0)
 
+# this is just to visualize data ...
+
+# omit the NAs that made their way into the dataframe
 growth <- na.omit(growth)
+
+# summarize response data as mean and standard error
 growth_summary <- growth %>% 
   group_by(site, b_r, outplant_time) %>% 
   summarize(av_length = mean(shell_length), se_length =
               sd(shell_length)/sqrt(length(shell_length)))
 
+# create a plot to visualize growth data over time
 growth_plot <- ggplot(aes(x = outplant_time, y = av_length, colour = site, shape = b_r),
                       data = growth_summary) +
   geom_point(size = 3) +
   ylab("Mean length (mm)") +
   xlab("Time since outplant (weeks)") +
   theme_classic() +
-  geom_smooth(method = "lm", se = FALSE, aes(linetype = b_r)) +
   geom_errorbar(aes(ymax = av_length + se_length, ymin = av_length - se_length))
-
 growth_plot
+
+# this is just me playing with the salinity temperature data to see how often
+# salinity and temperature conditions were stressful for oysters
 
 enviro <- read_csv("salinity_temperature.csv")
 
